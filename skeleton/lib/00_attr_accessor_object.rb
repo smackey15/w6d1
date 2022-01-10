8b0cb3c9d54ed1738a54faa713cf023b11ac4137
sean <midruss@yahoo.com>
2022-01-10 15:04:44 -0500

    Completed 00 my_attr_accessor

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,12 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+      define_method("#{name}=") do |set_value|
+        instance_variable_set("@#{name}", set_value)
+      end
+    end
   end
 end
